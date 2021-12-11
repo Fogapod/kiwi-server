33871251eb4c6408b2856f62b2740b12be998b47
Eugene <fogaprod@gmail.com>
2021-12-12 00:01:51 +0300

    draw text border

--- src/routes/ip.rs
+++ src/routes/ip.rs
@@ -3,21 +3,64 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
-use actix_web::get;
 use actix_web::web::Data;
-use actix_web::{web, HttpRequest, HttpResponse, Responder};
+use actix_web::{get, web, HttpRequest, HttpResponse, Responder};
 
 use rand::seq::IteratorRandom;
 
-use image::{GenericImageView, Rgba};
+use image::{DynamicImage, GenericImage, Rgba};
 
-use imageproc::drawing::draw_text_mut;
+use rusttype::{point, Font, PositionedGlyph, Rect, Scale};
 
-use rusttype::{point, Font, Scale};
+use imageproc::drawing::Canvas;
+
+// a modified version of:
+// https://github.com/silvia-odwyer/gdl/blob/421c8df718ad32f66275d178edec56ec653caff9/crate/src/text.rs#L23
+#[allow(clippy::too_many_arguments)]
+pub fn draw_text_with_border<'a>(
+    canvas: &mut DynamicImage,
+    x: u32,
+    y: u32,
+    scale: Scale,
+    font: &'a Font<'a>,
+    text: &str,
+    color: Rgba<u8>,
+    outline_color: Rgba<u8>,
+    outline_width: u8,
+) {
+    let mut background: DynamicImage = DynamicImage::new_luma8(canvas.width(), canvas.height());
+
+    imageproc::drawing::draw_text_mut(&mut background, color, x, y, scale, font, text);
+
+    let mut background = background.to_luma8();
+
+    imageproc::morphology::dilate_mut(
+        &mut background,
+        imageproc::distance_transform::Norm::LInf,
+        outline_width,
+    );
+
+    // Add a border to the text.
+    for x in 0..background.width() {
+        for y in 0..background.height() {
+            let pixval = 255 - background.get_pixel(x, y).0[0];
+            if pixval != 255 {
+                canvas.put_pixel(x, y, outline_color);
+            }
+        }
+    }
+
+    imageproc::drawing::draw_text_mut(canvas, color, x, y, scale, font, text);
+}
 
 // taken from https://github.com/image-rs/imageproc/pull/453
 // because it is not yet released
-fn layout_glyphs(scale: Scale, font: &Font, text: &str) -> (i32, i32) {
+fn layout_glyphs(
+    scale: Scale,
+    font: &Font,
+    text: &str,
+    mut f: impl FnMut(PositionedGlyph, Rect<i32>),
+) -> (i32, i32) {
     let v_metrics = font.v_metrics(scale);
 
     let (mut w, mut h) = (0, 0);
@@ -26,6 +69,7 @@ fn layout_glyphs(scale: Scale, font: &Font, text: &str) -> (i32, i32) {
         if let Some(bb) = g.pixel_bounding_box() {
             w = max(w, bb.max.x);
             h = max(h, bb.max.y);
+            f(g, bb);
         }
     }
 
@@ -35,13 +79,15 @@ fn layout_glyphs(scale: Scale, font: &Font, text: &str) -> (i32, i32) {
 // taken from https://github.com/image-rs/imageproc/pull/453
 // because it is not yet released
 pub fn text_size(scale: Scale, font: &Font, text: &str) -> (i32, i32) {
-    layout_glyphs(scale, font, text)
+    layout_glyphs(scale, font, text, |_, _| {})
 }
 
 #[get("")]
 async fn get_ip(req: HttpRequest, font: Data<Font<'_>>, cfg: Data<Config>) -> impl Responder {
     let conn_info = req.connection_info();
-    let text = conn_info.realip_remote_addr().unwrap_or("???");
+    let text = &conn_info
+        .realip_remote_addr()
+        .unwrap_or("unknown remote address");
 
     let files = fs::read_dir(&cfg.memes_path).expect("unable to read memes directory");
 
@@ -62,17 +108,19 @@ async fn get_ip(req: HttpRequest, font: Data<Font<'_>>, cfg: Data<Config>) -> im
 
     let rendered_text_size = text_size(scale, &font, text);
 
-    draw_text_mut(
+    draw_text_with_border(
         &mut image,
-        Rgba([255u8, 255u8, 255u8, 255u8]),
-        dim_x / 2 - min(rendered_text_size.0 as u32, dim_x) / 2,
+        dim_x / 2 - min(rendered_text_size.0, dim_x as i32) as u32 / 2,
         min(
             ((dim_y as f32 * 0.85) - rendered_text_size.1 as f32 * 0.5) as u32,
-            dim_y - min(rendered_text_size.1 as u32, dim_y),
+            dim_y - min(rendered_text_size.1, dim_y as i32) as u32,
         ),
         scale,
         &font,
         text,
+        Rgba([255u8, 255u8, 255u8, 255u8]),
+        Rgba([0u8, 0u8, 0u8, 255u8]),
+        2,
     );
 
     let mut bytes = vec![];
